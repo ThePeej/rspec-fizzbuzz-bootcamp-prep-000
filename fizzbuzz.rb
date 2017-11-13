7defd283dedf28f393befa709a731a4ac0b03fbc
PJ Frias <peejfrias@gmail.com>
2017-11-13 11:43:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz(num)
   if num%5 == 0 && num%3 == 0
     puts "FizzBuzz"
   elsif num%3 == 0
-    "Fizz"
+    puts "Fizz"
   elsif num%5 == 0
-    "Buzz"
+    puts "Buzz"
   else
     return nil
   end
