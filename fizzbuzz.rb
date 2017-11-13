e5b5b6f0a77e79bc9ff36ea7e4084a50a805aa88
PJ Frias <peejfrias@gmail.com>
2017-11-13 11:50:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
   if num%5 == 0 && num%3 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif num%3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif num%5 == 0
-    puts "Buzz"
+    "Buzz"
   else
     return nil
   end
